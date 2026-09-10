466e86cd4d54de4a09963853b8901bb45baf2386
feek3r <d.k.jablonski92@gmail.com>
2026-09-10 20:25:40 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const port=8080;
+
+
+const server = http.createServer((req,res)=>{
+
+
+
+})
+
+server.listen.apply(port,'localhost',()=>{
+    console.log(`Server running... at localhost:${port}'`)
+})
+
